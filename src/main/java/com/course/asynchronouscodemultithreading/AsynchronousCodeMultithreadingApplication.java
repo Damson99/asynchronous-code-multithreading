5cc642374f8c8ca5e9d8262c407510e2aad71ca2
dwieczorek <damian.wieczorek@spicasolutions.pl>
2021-08-15 17:33:04 +0200

    initial project add. #Closes 1

---
+++ src/main/java/com/course/asynchronouscodemultithreading/AsynchronousCodeMultithreadingApplication.java
@@ -0,0 +1,10 @@
+package com.course.asynchronouscodemultithreading;
+
+public class AsynchronousCodeMultithreadingApplication {
+
+	public static void main(String[] args)
+	{
+		System.out.println("ASD");
+	}
+
+}
